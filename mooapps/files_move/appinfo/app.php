c48612aca6211ee337436c83c1342b4e23f4b4dd
Lahmizzar Valeryon <lahmizzar@devxive.com>
2016-08-17 20:41:37 +0200

    initially add files move

---
+++ mooapps/files_move/appinfo/app.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * ownCloud - files_mv
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author eotryx <mhfiedler@gmx.de>
+ * @copyright eotryx 2015
+ */
+
+namespace OCA\Files_Mv\AppInfo;
+
+\OCP\Util::addScript( 'files_mv', "move" );
+\OCP\Util::addStyle('files_mv', 'mv');
+
---
+++ mooapps/files_move/l10n/es.js
@@ -0,0 +1,13 @@
+OC.L10N.register(
+    "oc_files_mv",
+    {
+    "No filesystem found" : "Ningún archivo de sistema encontrado",
+    "No data supplied." : "No se ha especificado los datos",
+    "Src and Dest are not allowed to be the same location!" : "¡Origen y destino no pueden estar en la misma localización!",
+    "Could not move %s - File with this name already exists" : "No se pudo mover %s - Ya existe un archivo con ese nombre.",
+    "Could not move %s" : "No se pudo mover %s",
+    "Move" : "Mover",
+    "Copy" : "Copiar",
+    "Destination directory" : "Directorio de destino"
+},
+"nplurals=2; plural=(n != 1);");
